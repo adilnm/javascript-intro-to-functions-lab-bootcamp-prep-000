5a6ef3c654c476617946785bceb29c9c27bf5712
ADIL NAIMI <adilnm23@hotmail.com>
2019-01-07 03:08:39 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ function sayHiToGrandma(string){
   {
     return "YES INDEED"
   }
-   else if (string===string.toUpperCase())
+   else if (string==="I love you, Grandma.")
   {
     return "YES INDEED"
   }
